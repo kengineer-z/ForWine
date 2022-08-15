f2a7ef9587b2984c3a41e83b1cce6f83aea4b050
kengineer-z <kenichiro.zaitsu@winescan.co.jp>
2022-08-15 18:04:06 +0900

    first storage

---
+++ Fof-rwinescan/Fof-rwinescan/Fof_rwinescanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fof_rwinescanApp.swift
+//  Fof-rwinescan
+//
+//  Created by 財津賢一郎 on 2022/08/15.
+//
+
+import SwiftUI
+
+@main
+struct Fof_rwinescanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
